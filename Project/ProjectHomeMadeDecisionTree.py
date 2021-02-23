d4abbe9864accba5e3b1294c233c557de3b396a7
Spencer Peterson <u0927156@utah.edu>
2021-02-23 12:25:06 -0700

    Created Project Folder

---
+++ Project/ProjectHomeMadeDecisionTree.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 23 12:23:14 2021
+
+@author: Spencer Peterson
+"""
+from DecisionTree import DecisionTree
+
+import os
+import numpy as np
+import pandas as pd
+
+
