3da5f9c2bd6b0ec65dcf4d1b96a052d2c8d028d7
Spencer Peterson <u0927156@utah.edu>
2021-03-02 13:05:52 -0700

    Added Forest Predictor
    Added forest predictor. Also predicted with single decision tree. Single decision tree with max_depth of 6 is most accurate currently.

--- Project/ProjectForest.py
+++ Project/ProjectForest.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Feb 23 12:38:54 2021
+Created on Tue Mar  2 12:40:17 2021
 
 @author: Spencer Peterson
 """
 
 import numpy as np
 import pandas as pd
-from sklearn import tree
+from sklearn.ensemble import RandomForestClassifier
 from sklearn import preprocessing
 from sklearn.metrics import accuracy_score
-
 
 train = 'train_final.csv'
 test = 'test_final.csv'
@@ -31,21 +30,22 @@ for i in range(14):
 Y = df.iloc[:,-1]
 X = df.iloc[:,0:14]
 
-decTree = tree.DecisionTreeClassifier(max_depth=6)
-decTree = decTree.fit(X,Y)
-
-test_predictions = decTree.predict(X)
-print(accuracy_score(Y, test_predictions))
+decForest = RandomForestClassifier(max_depth=4, n_estimators=200)
+decForest = decForest.fit(X,Y)
 
 testDF = pd.read_csv(test)
+
+
+test_predictions = decForest.predict(X)
+print(accuracy_score(Y, test_predictions))
+
 
 for i in range(1,15):
     testDF.iloc[:,i] = labelProcessor.fit_transform(testDF.iloc[:,i])
     
 
-predictions = decTree.predict(testDF.iloc[:,1:15])
-
+predictions = decForest.predict(testDF.iloc[:,1:15])
 
 predictionDF = pd.DataFrame(predictions)
 predictionDF["ID"] = testDF["ID"].values
-predictionDF.to_csv('Predictions_1.csv', index=False, header=['Prediction','ID'])
+predictionDF.to_csv('Predictions_2.csv', index=False, header=['Prediction','ID'])
